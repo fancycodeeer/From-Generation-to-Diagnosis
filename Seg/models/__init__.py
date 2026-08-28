0364f9ebe4698918bbe014a6ead24914e7c3dd0c
fancycodeeer <WANG1772@e.ntu.edu.sg>
2026-08-28 18:04:59 +0800

    Delete Seg directory

--- Seg/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Unet import *
-from .CoordinateAttention import *
-from .cyclegan import *
-from .threshold_pred import *
-from .FreqFusion import *
-from .Attention import *
-# from .MogaNet import *
-from .ResNet import *
-# from .ConvNeXt import *
